825966445aabf032c81f50f2f95e28d56240546f
xinming <xinming.lxj@antfin.com>
2021-01-04 15:08:09 +0800

    feat: interval support cornerRadius

--- src/geometry/shape/interval/util.ts
+++ src/geometry/shape/interval/util.ts
@@ -3,6 +3,33 @@ import { isArray, isNil } from '@antv/util';
 import { PathCommand } from '../../../dependents';
 import { Point, ShapePoint } from '../../../interface';
 
+export function parseRadius(radius) {
+  let r1 = 0;
+  let r2 = 0;
+  let r3 = 0;
+  let r4 = 0;
+  if (isArray(radius)) {
+    if (radius.length === 1) {
+      r1 = r2 = r3 = r4 = radius[0];
+    } else if (radius.length === 2) {
+      r1 = r3 = radius[0];
+      r2 = r4 = radius[1];
+    } else if (radius.length === 3) {
+      r1 = radius[0];
+      r2 = r4 = radius[1];
+      r3 = radius[2];
+    } else {
+      r1 = radius[0];
+      r2 = radius[1];
+      r3 = radius[2];
+      r4 = radius[3];
+    }
+  } else {
+    r1 = r2 = r3 = r4 = radius;
+  }
+  return [r1, r2, r3, r4];
+}
+
 /**
  * @ignore
  * 根据数据点生成矩形的四个关键点
@@ -91,7 +118,12 @@ export function getRectPath(points: Point[], isClosed: boolean = true): PathComm
  * @param coor 坐标
  * @returns 返回矩形的 path
  */
-export function getIntervalRectPath(points: Point[], lineCap: CanvasLineCap, coor: Coordinate): PathCommand[] {
+export function getIntervalRectPath(
+  points: Point[],
+  lineCap: CanvasLineCap,
+  coor: Coordinate,
+  style?: { radius?: number | number[] }
+): PathCommand[] {
   const width = coor.getWidth();
   const height = coor.getHeight();
   const isRect = coor.type === 'rect';
@@ -112,6 +144,27 @@ export function getIntervalRectPath(points: Point[], lineCap: CanvasLineCap, coo
       path.push(['L', points[3].x, points[3].y]);
       path.push(['A', r, r, 0, 0, 1, points[0].x, points[0].y]);
     }
+    path.push(['z']);
+  } else if (style?.radius) {
+    const [r1, r2, r3, r4] = parseRadius(style?.radius);
+    const converted = points.map((p) => coor.convert(p));
+
+    path.push(['M', converted[0].x, converted[0].y - r1]);
+    path.push(['L', converted[1].x, converted[1].y + r2]);
+    r2 !== 0 && path.push(['A', r2, r2, 0, 0, 1, converted[1].x + r2, converted[1].y]);
+    path.push(['L', converted[2].x - r3, converted[2].y]);
+    r3 !== 0 && path.push(['A', r3, r3, 0, 0, 1, converted[2].x, converted[2].y + r3]);
+    path.push(['L', converted[3].x, converted[3].y - r4]);
+    r4 !== 0 && path.push(['A', r4, r4, 0, 0, 1, converted[3].x - r4, converted[3].y]);
+    path.push(['L', converted[0].x + r1, converted[0].y]);
+    r1 !== 0 && path.push(['A', r1, r1, 0, 0, 1, converted[0].x, converted[0].y - r1]);
+
+    path.forEach((p) => {
+      const [x, y] = p.slice(-2);
+      const inverted = coor.invertPoint({ x, y });
+      p.splice(-2, 2, inverted.x, inverted.y);
+    });
+
     path.push(['z']);
   } else {
     path = getRectPath(points);
